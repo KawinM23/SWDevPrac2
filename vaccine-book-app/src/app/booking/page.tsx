2070c1f5060fa775c631b2a58a5583a2b13edcc5
KawinM23 <kawin.m235@gmail.com>
2023-09-04 16:59:32 +0700

    Week05-Assignment

--- vaccine-book-app/src/app/booking/page.tsx
+++ vaccine-book-app/src/app/booking/page.tsx
@@ -3,7 +3,7 @@ import Form from "@/components/Form";
 export default function page() {
   return (
     <main className="w-full flex flex-col items-center space-y-4">
-      <h1 className="mt-8">Vaccination Booking</h1>
+      <h1 className="mt-8 text-blue-900">Vaccination Booking</h1>
       <Form />
     </main>
   );
