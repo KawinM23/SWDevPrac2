c7a31e6f066723720b045e3e213baa5acc2fc353
KawinM23 <kawin.m235@gmail.com>
2023-09-12 21:42:39 +0700

    Add more banner image

--- vaccine-book-app/src/components/Banner.tsx
+++ vaccine-book-app/src/components/Banner.tsx
@@ -1,16 +1,29 @@
-import styles from "./banner.module.css";
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Banner() {
+  const [index, setIndex] = useState(0);
+
+  const imgSrc = [
+    "/img/cover1.avif",
+    "/img/cover2.avif",
+    "/img/cover3.avif",
+    "/img/cover4.avif",
+  ];
+
   return (
     <div className="block h-[60vh] relative">
       <Image
-        src={"/img/cover.avif"}
+        src={imgSrc[index]}
         alt="cover"
         fill={true}
         objectFit="cover"
         priority
         className="opacity-60"
+        onClick={() => {
+          setIndex((index + 1) % imgSrc.length);
+        }}
       />
       <div className="z-20 top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 text-3xl font-bold text-[#142680]  absolute text-center ">
         <h1 className="text-5xl">Vaccination Service</h1>
